c4b006a2489ccdaebd93ca024e304c9dd7a7353f
rishabhiitbhu <rishabh.agrahari.eee15@iitbhu.ac.in>
2016-09-25 13:29:35 +0530

    mail content modified

--- mt.py
+++ mt.py
@@ -9,9 +9,9 @@ requests.post(
         "https://api.mailgun.net/v3/mg.technex.in/messages",
         auth=("api", "key-cf7f06e72c36031b0097128c90ee896a"),
 
-        data={"from": "Excited User <YOU@YOUR_DOMAIN_NAME>",
+        data={"from": "Publicity Team Technex'17<publicity@technex.in>",
               "to": "rishabh.agrahari.eee15@iitbhu.ac.in",
-              "subject": "Hello",
+              "subject": "Technex CA Dashboard live!",
               "html": """
                     <html>
                       <head>
@@ -19,17 +19,24 @@ requests.post(
                       <body>
                         </div>
                         <div >
-                          <h1 >Hi Technex'17 Campus Ambassadors!</h1>
+                          <h1>Hi %s!</h1>
                           <p>
                           <h3>The Dashboard is live!!</h3>
+                          <h3> Visit your dashboard to complete your first task.</h3>
                           </p>
-                          <hr >
                           <p>
-                          <a class="btn btn-primary btn-lg" href="http://ca.technex.in" role="button">Technex'17 CA portal</a>
+                          <a class="btn btn-primary btn-lg" href="http://ca.technex.in" role="button">Technex'17 CA Dashboard.</a>
+                          (http://ca.technex.in/login)
                           </p>
+                          <hr>
+                          <br>
+                          <p>
+                          Regards<br>
+                          Publicity Team <br>
+                          Technex'17 <br>
                         </div>
                       </body>
                     </html>
-                    """
+                    """ %('rishabh')
                     }
         )
